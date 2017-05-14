cad573149adcb61e26ad0b37f0a41d619f5db38a
cekk <andrea.cecchi85@gmail.com>
2017-05-15 00:05:23 +0200

    fix test

--- config/settings.js
+++ config/settings.js
@@ -7,8 +7,8 @@ const program = require('commander');
 
 const DB_URL = process.env.NODE_ENV !== 'production' ? 'localhost:5432' : process.env.DB_URL;
 const DB_NAME = process.env.DB_NAME !== undefined ? process.env.DB_NAME : 'rtkb';
-const DB_USER = process.env.DB_USER !== undefined ? process.env.DB_USER : 'cekk';
-const DB_PASSWORD = process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : 'cekk';
+const DB_USER = process.env.DB_USER !== undefined ? process.env.DB_USER : 'postgres';
+const DB_PASSWORD = process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : '';
 
 var options = {
     rootPath: path.normalize(__dirname + '/../'),
