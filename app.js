e42f05ffbe1d9fc0ae9ea75b8b95c8385f3effe7
cekk <andrea.cecchi85@gmail.com>
2017-05-15 00:00:33 +0200

    refactoring and changed db

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Glue = require('glue');
 const chalk = require('chalk');
 const manifest = require('./config/manifest.json');
-const mongoose = require('mongoose');
+const models = require('./lib/models');
 // const glob = require('glob');
 const config = require('./config/settings');
 const Labbable = require('labbable');
@@ -11,28 +11,6 @@ const options = {
   relativeTo: __dirname + '/lib/modules'
 };
 
-// Bootstrap db connection
-var db = mongoose.connect(config.db.url + '/' + config.db.name, function (err) {
-    if (err) {
-        console.error(chalk.red('[ERROR] Could not connect to MongoDB!'));
-        console.log(chalk.red(err));
-        process.exit(1);
-    }
-    else {
-      console.log(chalk.green('[DB] Connected to ' + config.db.url + '/' + config.db.name));
-    }
-});
-
-// // Bootstrap models
-const Server =  require('./lib/modules/servers/models');
-
-
-// var models = glob.sync(config.rootPath + "models/**/*.js");
-// models.forEach(function (modelPath) {
-//     require(path.resolve(modelPath));
-// });
-
-// the server
 Glue.compose(manifest, options, function (err, server) {
   // Show the server to our instance of labbable
   labbable.using(server);
@@ -42,5 +20,3 @@ Glue.compose(manifest, options, function (err, server) {
     }
   });
 });
-
-exports.db = db;
--- config/settings.js
+++ config/settings.js
@@ -5,13 +5,16 @@
 const path = require('path');
 const program = require('commander');
 
-const DB_PATH = process.env.NODE_ENV !== 'production' ? 'localhost:27017' : process.env.DB_PATH;
+const DB_URL = process.env.NODE_ENV !== 'production' ? 'localhost:5432' : process.env.DB_URL;
+const DB_NAME = process.env.DB_NAME !== undefined ? process.env.DB_NAME : 'rtkb';
+const DB_USER = process.env.DB_USER !== undefined ? process.env.DB_USER : 'cekk';
+const DB_PASSWORD = process.env.DB_PASSWORD !== undefined ? process.env.DB_PASSWORD : 'cekk';
 
 var options = {
     rootPath: path.normalize(__dirname + '/../'),
     db: {
-        url: 'mongodb://' + DB_PATH,
-        name: process.env.DB_NAME || 'rtkb'
+        url: `postgres://${DB_USER}:${DB_PASSWORD}@${DB_URL}/${DB_NAME}`,
+        name: DB_NAME,
     },
 };
 
